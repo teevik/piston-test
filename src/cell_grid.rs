860f2ed2ecaf65ad6a6c45aee66ba7d769924864
teevik <teemu.vikoren@gmail.com>
2020-09-13 00:50:59 +0200

    ...stuff

--- src/cell_grid.rs
+++ src/cell_grid.rs
@@ -6,15 +6,16 @@ use vecmath::{Vector2, vec2_add, vec2_cast};
 use gfx_device_gl::{Factory, Resources, CommandBuffer, Device};
 use piston_window::math::Matrix2d;
 use crate::live_cell_states::sand_cell_state::SandCellState;
-use itertools::Itertools;
+use itertools::{Itertools, Chunk};
+use std::f32::INFINITY;
 
 #[derive(Copy, Clone)]
-pub struct LiveCellApi<'a> {
+pub struct LiveCellApi {
     pub tile_position: Vector2<u32>,
-    pub chunks: [[&'a CellChunk; 3]; 3]
+    pub chunks: [[Option<[[Cell; CHUNK_SIZE]; CHUNK_SIZE]>; 3]; 3]
 }
 
-impl<'a> LiveCellApi<'a> {
+impl LiveCellApi {
     pub fn get_cell(self, tile_offset: Vector2<i32>) -> Cell {
         let [target_tile_x, target_tile_y] = vec2_add([self.tile_position[0] as i32, self.tile_position[1] as i32], tile_offset);
         
@@ -26,7 +27,11 @@ impl<'a> LiveCellApi<'a> {
 
         let chunk = self.chunks[(chunk_x + 1) as usize][(chunk_y + 1) as usize];
         
-        chunk.get_cell([relative_target_tile_x, relative_target_tile_y])
+        if let Some(chunk) = chunk {
+            return chunk[relative_target_tile_x as usize][relative_target_tile_y as usize]
+        }
+        
+        return Cell::Static(StaticCell::new(Rgba([0, 0, 0, 0])))
     }
     
     pub fn is_empty(self, tile_offset: Vector2<i32>) -> bool {
@@ -36,15 +41,15 @@ impl<'a> LiveCellApi<'a> {
 
 #[derive(Copy, Clone)]
 pub struct LiveCellInstructions {
-    move_instruction: Option<LiveCellMoveInstruction>,
-    new_color: Option<Rgba<u8>>
+    pub move_instruction: Option<LiveCellMoveInstruction>,
+    pub new_color_instruction: Option<Rgba<u8>>
 }
 
 impl LiveCellInstructions {
     pub fn new() -> Self {
         Self {
             move_instruction: None,
-            new_color: None
+            new_color_instruction: None
         }
     }
     
@@ -53,8 +58,8 @@ impl LiveCellInstructions {
         self
     }
 
-    pub fn with_new_color(&mut self, new_color: Option<Rgba<u8>>) -> &mut Self {
-        self.new_color = new_color;
+    pub fn with_new_color_instruction(&mut self, new_color_instruction: Option<Rgba<u8>>) -> &mut Self {
+        self.new_color_instruction = new_color_instruction;
         self
     }
 }
@@ -170,32 +175,67 @@ impl CellChunk {
         piston_window::image(&self.texture, transform, graphics);
     }
     
-    pub fn update(&mut self, current_frame: u64, neighbor_chunks: [&mut CellChunk; 8]) {
+    pub fn update(&mut self, current_frame: u64, neighbor_chunks: [Option<&mut CellChunk>; 8]) {
         let is_even = current_frame % 2 == 0;
 
-        let chunks: [[&CellChunk; 3]; 3] = [
-            [neighbor_chunks[0], neighbor_chunks[1], neighbor_chunks[2]],
-            [neighbor_chunks[3], self, neighbor_chunks[4]],
-            [neighbor_chunks[5], neighbor_chunks[6], neighbor_chunks[7]]
-        ];
-
-        for x in if is_even { CHUNK_SIZE..0 } else { 0..CHUNK_SIZE } {
-            for y in CHUNK_SIZE..0 {
-                let cell = &mut self.cells[x][y];
-                
-                if let Cell::Live(live_cell) = cell {
-                    if live_cell.last_frame_updated == current_frame {
-                        continue;
-                    }
-                    
-                    
-                    let live_cell_instructions = live_cell.state.update(LiveCellApi {
-                        tile_position: [x as u32, y as u32],
-                        chunks
-                    });
-                }
-            }  
-        }
+        // let chunks: [[Option<[[Cell; CHUNK_SIZE]; CHUNK_SIZE]>; 3]; 3] = [
+        //     [neighbor_chunks[0].map(|a| a.cells), neighbor_chunks[1].map(|a| a.cells), neighbor_chunks[2].map(|a| a.cells)],
+        //     [neighbor_chunks[3].map(|a| a.cells), Some(self.cells), neighbor_chunks[4].map(|a| a.cells)],
+        //     [neighbor_chunks[5].map(|a| a.cells), neighbor_chunks[6].map(|a| a.cells), neighbor_chunks[7].map(|a| a.cells)]
+        // ];
+        // 
+        // for x in if is_even { (CHUNK_SIZE - 1)..=1 } else { 0..=(CHUNK_SIZE - 1) } {
+        //     for y in (CHUNK_SIZE - 1)..=0 {
+        //         let cell = &mut self.cells[x][y];
+        //         
+        //         let tile_position = [x as i32, y as i32];
+        //         
+        //         if let Cell::Live(live_cell) = cell {
+        //             if live_cell.last_frame_updated == current_frame {
+        //                 continue;
+        //             }
+        //             
+        //             let live_cell_instructions = live_cell.state.update(LiveCellApi {
+        //                 tile_position: [x as u32, y as u32],
+        //                 chunks
+        //             });
+        //             
+        //             let move_instruction = live_cell_instructions.move_instruction;
+        //             let new_color_instruction = live_cell_instructions.new_color_instruction;
+        //             
+        //             if let Some(move_instruction) = move_instruction {
+        //                 match move_instruction {
+        //                     LiveCellMoveInstruction::Replace(tile_offset) => {
+        //                         let [target_tile_x, target_tile_y] = vec2_add(tile_position, tile_offset);
+        // 
+        //                         let chunk_x = ((target_tile_x as f32) / (CHUNK_SIZE as f32)).floor() as i32;
+        //                         let chunk_y = ((target_tile_y as f32) / (CHUNK_SIZE as f32)).floor() as i32;
+        // 
+        //                         let relative_target_tile_x = (target_tile_x + (chunk_x * (CHUNK_SIZE as i32))) as u32;
+        //                         let relative_target_tile_y = (target_tile_y + (chunk_y * (CHUNK_SIZE as i32))) as u32;
+        // 
+        //                         
+        //                         
+        //                         // let chunk = self.chunks[(chunk_x + 1) as usize][(chunk_y + 1) as usize];
+        //                         // 
+        //                         // if let Some(chunk) = chunk {
+        //                         //     return chunk[relative_target_tile_x as usize][relative_target_tile_y as usize]
+        //                         // }
+        // 
+        //                         // return Cell::Static(StaticCell::new(Rgba([0, 0, 0, 0])))
+        //                         // if let Some(a) = neighbor_chunks[0] {
+        //                         //     a.cells[0][0] = Cell::Empty;
+        //                         // }
+        //                     },
+        //                     
+        //                     LiveCellMoveInstruction::Switch(tile_offset) => {
+        //                         
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     }  
+        // }
     }
 }
 
@@ -244,13 +284,46 @@ impl CellGrid {
     
     pub fn update(&mut self, current_frame: u64) {
         let even_frame = current_frame % 2 == 0;
-        
-        let a = self.chunks.iter_mut()
-            .sorted_by_key(|([x, y], _)| [-y, if even_frame {-x} else {*x} ]);
 
-        for (chunk_position, chunk) in a {
+        fn get_chunk_at_offset(chunks: &mut HashMap<Vector2<i32>, CellChunk>, chunk_position: Vector2<i32>, offset: Vector2<i32>) -> Option<&mut CellChunk> {
+            let a = vec2_add(chunk_position, offset);
             
+            chunks.get_mut(&a)
         }
 
+        let mut min_x_chunk_position = i32::MAX;
+        let mut min_y_chunk_position = i32::MAX;
+        let mut max_x_chunk_position = i32::MIN;
+        let mut max_y_chunk_position = i32::MIN;
+
+        for [chunk_position_x, chunk_position_y] in self.chunks.keys() {
+            if *chunk_position_x < min_x_chunk_position { min_x_chunk_position = *chunk_position_x }
+            if *chunk_position_x > max_x_chunk_position { max_x_chunk_position = *chunk_position_x }
+            if *chunk_position_y < min_y_chunk_position { min_y_chunk_position = *chunk_position_y }
+            if *chunk_position_y > max_y_chunk_position { max_y_chunk_position = *chunk_position_y }
+        }
+
+        let x_range = if even_frame { min_x_chunk_position .. max_x_chunk_position } else { max_x_chunk_position .. min_x_chunk_position };
+        
+        for chunk_position_x in x_range {
+            for chunk_position_y in max_y_chunk_position .. min_y_chunk_position {
+                let chunk_position = [chunk_position_x, chunk_position_y];
+                
+                let chunk = self.chunks.get_mut(&chunk_position);
+                
+                if let Some(chunk) = chunk {
+                    chunk.update(current_frame, [
+                        get_chunk_at_offset(&mut self.chunks, chunk_position, [-1, -1]),
+                        get_chunk_at_offset(&mut self.chunks, chunk_position, [-1, -1]),
+                        get_chunk_at_offset(&mut self.chunks, chunk_position, [-1, -1]),
+                        get_chunk_at_offset(&mut self.chunks, chunk_position, [-1, -1]),
+                        get_chunk_at_offset(&mut self.chunks, chunk_position, [-1, -1]),
+                        get_chunk_at_offset(&mut self.chunks, chunk_position, [-1, -1]),
+                        get_chunk_at_offset(&mut self.chunks, chunk_position, [-1, -1]),
+                        get_chunk_at_offset(&mut self.chunks, chunk_position, [-1, -1])
+                    ])
+                }
+            }
+        }
     }
 }
